6cd9bfe85673ad9d45ee709aa09457936558a93b
GurveerSingh01 <gurveersingh22122005@gmail.com>
2025-01-27 14:58:47 -0600

    Update final PieStat.java
    better indentation

--- PieStat.java
+++ PieStat.java
@@ -12,20 +12,17 @@ public class PieData
         Scanner f = new Scanner(new File("A1Data.txt"));
         Scanner ff = new Scanner(new File("A1Data.txt"));
         Scanner kb=new Scanner(System.in);
-        
-        int count=0;
-        
+
+        int count=0;        
         String summaryResult="";
-        //Scanner line = new Scanner(f.nextLine()); 
-        //System.out.println(f.next());
 
         count= linesCount(ff,count);
-        
+
         String names[] = new String[count];
         int bib[]= new int[count];
         int pieTimes[][]= new int[count][10];
         int pieVolumes[][] = new int[count][3];
-        
+
         double avgTime[] = new double[count];
         double avgVol[] = new double[count];
         double weightedScore[] = new double[count];
@@ -42,23 +39,19 @@ public class PieData
         calculateRank(weightedScore,count,ranks);
 
         System.out.println("PPEA Individual Statistics \n*********************************\n");
-        
-        
-        summaryResult= displaySummary(names,bib,pieTimes,pieVolumes,count, avgTime,avgVol,weightedScore,ranks);
-         System.out.print(summaryResult);
-         
-         
+
+        summaryResult = displaySummary(names,bib,pieTimes,pieVolumes,count, avgTime,avgVol,weightedScore,ranks);
+        System.out.print(summaryResult);
+
         System.out.println("\n******* Edit Records*******");
         System.out.println("\nChoose an option:\nA - Add a score\nB - Quit");
         String option=kb.next();
-        // 
 
         while(option.equalsIgnoreCase("a"))
         {
-
-            System.out.println(option);
             System.out.println("\nEnter the bib number of the competitor to edit.");
             int userBib = kb.nextInt(); 
+
             int validIndex = getIndexToEdit(userBib,bib,count);
 
             while(validIndex==-1)
@@ -71,21 +64,19 @@ public class PieData
 
             System.out.println("\nSelect:\nT: add a time\nV: add a volume");
             String optionEdit=kb.next();
+
             addScore(optionEdit,validIndex,pieTimes,count,avgTime,avgVol,weightedScore,ranks,pieVolumes,kb);
 
+            summaryResult= displaySummary(names,bib,pieTimes,pieVolumes,count, avgTime,avgVol,weightedScore,ranks);
+            System.out.println(summaryResult);
 
-           summaryResult= displaySummary(names,bib,pieTimes,pieVolumes,count, avgTime,avgVol,weightedScore,ranks);
-           System.out.println(summaryResult);
-          
-          
             System.out.println("****************************************************");
             System.out.println("\nChoose an option:\nA - Add a score\nB - Quit");
             option=kb.next();
         }
         System.out.println("\nend of program");
-
     }
-    
+
     public static int linesCount(Scanner ff,int count)
     {
         while(ff.hasNextLine())
@@ -95,10 +86,6 @@ public class PieData
         }
         return count;
     }
-
-   
-
-    
 
     public static void fillStartingData(Scanner f,int count,String names[],int bib[], int pieTimes[][],int pieVolumes[][])
     {
@@ -140,6 +127,7 @@ public class PieData
                     minIndex= j;
                 }
             }
+
             for(int k=0;k<10;k++)
             {
                 if(k!=maxIndex && k!=minIndex)
@@ -147,14 +135,17 @@ public class PieData
                     avTime= avTime + pieTimes[i][k];
                 }
             }
+
             avTime=avTime/8;
             avgTime[i]=avTime;
+            // Converted to 2decimals places in the output displayed in displaySummary
         }
     }
 
     public static void calculateAverageVolume(int pieVolumes[][],int count, double avgVol[])
     {
         double avVol=0;
+
         for(int i=0;i<count;i++)
         {
             avVol=0;
@@ -162,14 +153,17 @@ public class PieData
             {
                 avVol = avVol + (double)pieVolumes[i][j];
             }
+
             avVol = avVol/3;
             avgVol[i] = avVol;
+            // Converted to 2decimals places in the output displayed in display summary
         }
     }
 
     public static void calculateWeightedScore(double avgTime[],double avgVol[],double weightedScore[],int count)
     {
         double wScore=0;
+
         for(int i=0;i<count;i++)
         {
             wScore=0;
@@ -182,6 +176,7 @@ public class PieData
     {
         int counter;
         double currentMax=0;
+
         for(int i=0;i<count;i++)
         {
             counter=1;
@@ -196,7 +191,8 @@ public class PieData
         }
 
     }
-     public static int getIndexToEdit(int userBib, int bib[],int count)
+
+    public static int getIndexToEdit(int userBib, int bib[],int count)
     {
         int validIndex = -1;
         for(int i=0;i<count;i++)
@@ -212,16 +208,18 @@ public class PieData
 
     public static void addScore(String optionEdit,int validIndex,int pieTimes[][],int count,double avgTime[],double avgVol[],double weightedScore[],int ranks[],int pieVolumes[][],Scanner sc)
     {
+        int score=0;
         if(optionEdit.equalsIgnoreCase("t"))
         {
             System.out.println("\nEnter the new score value.");
-            int score=sc.nextInt();
+            score=sc.nextInt();
 
             for(int j=0;j<=8;j++)
             {
                 pieTimes[validIndex][j]=pieTimes[validIndex][j+1];                      
             }
             pieTimes[validIndex][9]=score;
+            
             calculateAverageTime(pieTimes,count,avgTime);
             calculateWeightedScore(avgTime,avgVol,weightedScore,count);
             calculateRank(weightedScore,count,ranks);
@@ -229,13 +227,14 @@ public class PieData
         else if(optionEdit.equalsIgnoreCase("v"))
         {
             System.out.println("\nEnter the new score value.");
-            int score=sc.nextInt();
+            score=sc.nextInt();
 
             for(int j=0;j<2;j++)
             {
                 pieVolumes[validIndex][j]=pieVolumes[validIndex][j+1];                            
             }
             pieVolumes[validIndex][2]=score;
+            
             calculateAverageVolume(pieVolumes,count,avgVol);
             calculateWeightedScore(avgTime,avgVol,weightedScore,count);
             calculateRank(weightedScore,count,ranks);
@@ -248,34 +247,26 @@ public class PieData
     {
         int x1=0;
         StringBuilder result= new StringBuilder();
-       
-        //System.out.println("Name\tBib#\tAvgTime\tV1\tV2\tV3\tAvgVol\tScore\tRank");
+
         result.append("Name\tBib#\tAvgTime\tV1\tV2\tV3\tAvgVol\tScore\tRank\n");
         while(x1<count)
         {
-             result.append(names[x1]+"\t");
-             result.append(+bib[x1]+"\t");
-             result.append(String.format("%.2f", avgTime[x1])+"\t");
+            result.append(names[x1]+"\t");
+            result.append(+bib[x1]+"\t");
+            result.append(String.format("%.2f", avgTime[x1])+"\t");
 
-            /**for(int j=7;j<10;j++)
-            {
-            System.out.print("\t"+pieTimes[x1][j]+" ");
-            }
-             */
             for(int j=0;j<3;j++)
             {
-                 result.append(pieVolumes[x1][j]+"\t");
+                result.append(pieVolumes[x1][j]+"\t");
             }
 
             result.append(String.format("%.2f",avgVol[x1])+"\t");
-             result.append(String.format("%.2f", weightedScore[x1])+"\t");
-             result.append( ranks[x1]+"\t");
+            result.append(String.format("%.2f", weightedScore[x1])+"\t");
+            result.append( ranks[x1]+"\t");
 
             result.append("\n");
             x1++;
         }
         return result.toString();
-
     }
-
 }
